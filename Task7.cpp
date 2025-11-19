4afba4c1fee81e70568ce3967007aa04819db653
16ziyoda <z.xakimjonova@newuu.uz>
2025-11-19 23:07:35 +0500

    Create Task7.cpp

---
+++ Task7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int *pa = &a, *pb = &b;
+    int product = (*pa) * (*pb);
+    cout << product << '\n';
+    return 0;
+}
